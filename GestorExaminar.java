86e358f788f3ab36e942d5fdf93bfb4f0db8f0cd
graprisco <50212960+graprisco@users.noreply.github.com>
2019-05-03 19:17:35 +0200

    Top2 y controlado

--- GestorExaminar.java
+++ GestorExaminar.java
@@ -2,32 +2,64 @@ package controlador;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+
+import vista.Top;
 
 public class GestorExaminar implements ActionListener {
 
-	JPanel jp;
+	private JButton jbExaminar;
+	private Top vTop;
 	
-	public GestorExaminar(JPanel jp) {
+	public GestorExaminar(JButton jbExaminar) {
 		
-		this.jp = jp;
+		this.jbExaminar = jbExaminar;
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		
-		JFileChooser selector = new JFileChooser();
-		ImgFileFilter filtro = new ImgFileFilter();
-		selector.setFileFilter(filtro);
-		int returnVal = selector.showOpenDialog (null);
-		if (returnVal == JFileChooser.APPROVE_OPTION) {
-			System.out.println("Se eligió el archivo " + selector.getSelectedFile().getName());
+		vTop = new Top();
+		
+		//Creamos el objeto JFileChooser
+		JFileChooser fc = new JFileChooser();
+		 
+		//Abrimos la ventana, guardamos la opcion seleccionada por el usuario
+		int seleccion = fc.showOpenDialog(vTop);
+		 
+		//Si el usuario, pincha en aceptar
+		if(seleccion==JFileChooser.APPROVE_OPTION){
+		 
+		    //Seleccionamos el fichero
+		    File fichero = fc.getSelectedFile();
+		 
+		    //Ecribe la ruta del fichero seleccionado en el campo de texto
+		    vTop.getTextField().setText(fichero.getAbsolutePath());
+		 
+		    try(FileReader fr=new FileReader(fichero)){
+		        String cadena="";
+		        int valor=fr.read();
+		        while(valor!=-1){
+		            cadena=cadena+(char)valor;
+		            valor=fr.read();
+		        }
+		        textArea.setText(cadena);
+		   
+		    } catch (IOException e1) {
+		    
+		    	e1.printStackTrace();
+		    }
 		}
-			
 	}
-	
 
 }
--- Top.java
+++ Top.java
@@ -18,10 +18,11 @@ public class Top extends JPanel {
 		
 		textField = new JTextField();
 		textField.setPreferredSize(new Dimension(100,50));
+		textField.setEditable(false);
 		jbExaminar = new JButton("Examinar");
 		jbExaminar.setPreferredSize(new Dimension(150,25));
 		
-		jbExaminar.addActionListener(new GestorExaminar(this));
+		jbExaminar.addActionListener(new GestorExaminar(jbExaminar));
 	
 		
 		setLayout(new FlowLayout());
@@ -30,4 +31,21 @@ public class Top extends JPanel {
 		this.add(jbExaminar);
 			
 	}
+	
+	
+	
+	public JTextField getTextField() {
+		return textField;
+	}
+
+
+
+	public void setTextField (String textField) {
+		this.textField.setText(textField);
+	}
+	
+	public JButton getjbExaminar() {
+		return jbExaminar;
+	}
+
 }
